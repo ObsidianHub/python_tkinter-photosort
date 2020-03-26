912e2f0e5f64a90be91c505fcf2752342878835b
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-26 23:43:04 +0200

    create base structure in project (just package imports and create root app)

---
+++ photosort.py
@@ -0,0 +1,13 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+import os
+from datetime import datetime
+
+root = Tk()
+root.title('PhotoSort')
+root.geometry('500x150')
+
+
+
+root.mainloop()
