c81d3b183d777cec4e3db610f69a8ed2d1f942bf
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-26 23:44:47 +0200

    create base Frame and init empty functions for future Buttons

--- photosort.py
+++ photosort.py
@@ -4,10 +4,17 @@ from tkinter import messagebox
 import os
 from datetime import datetime
 
+def choose_dir():
+  pass
+
+def f_start():
+  pass
+
 root = Tk()
 root.title('PhotoSort')
 root.geometry('500x150')
 
-
+frame = Frame(root, bg="#56ADFF")
+frame.pack()
 
 root.mainloop()
