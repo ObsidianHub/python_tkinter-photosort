d48302a8bfd8d0039034f0bac0a2b53b63e6ea91
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-26 23:49:44 +0200

    a little of edits all our element (visual)

--- photosort.py
+++ photosort.py
@@ -12,18 +12,18 @@ def f_start():
 
 root = Tk()
 root.title('PhotoSort')
-root.geometry('500x150')
+root.geometry('500x150+1000+300')
 
-frame = Frame(root, bg="#56ADFF")
-frame.pack()
+frame = Frame(root, bg="#56ADFF", bd=5)
+frame.pack(pady=10, padx=10, fill=X)
 
 e_path = Entry(frame)
-e_path.pack(side=LEFT, expand=True, fill=X)
+e_path.pack(side=LEFT, ipady=2, expand=True, fill=X)
 
 btn_dialog = Button(frame, text="Выбрать папку", command=choose_dir)
-btn_dialog.pack(side=LEFT)
+btn_dialog.pack(side=LEFT, padx=5)
 
 btn_start = Button(root, text="Start", command=f_start)
-btn_start.pack(side=LEFT)
+btn_start.pack(fill=X, padx=10)
 
 root.mainloop()
