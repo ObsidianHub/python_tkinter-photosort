e40e94a1e5cdffad77002e736765d696b3311f7d
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-03-26 23:55:53 +0200

    add new style to our element by used ttk package

--- photosort.py
+++ photosort.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
+from tkinter import ttk
 import os
 from datetime import datetime
 
@@ -14,16 +15,19 @@ root = Tk()
 root.title('PhotoSort')
 root.geometry('500x150+1000+300')
 
+s = ttk.Style()
+s.configure('my.TButton', font=("Helvatica", 15))
+
 frame = Frame(root, bg="#56ADFF", bd=5)
 frame.pack(pady=10, padx=10, fill=X)
 
-e_path = Entry(frame)
+e_path = ttk.Entry(frame)
 e_path.pack(side=LEFT, ipady=2, expand=True, fill=X)
 
-btn_dialog = Button(frame, text="Выбрать папку", command=choose_dir)
+btn_dialog = ttk.Button(frame, text="Выбрать папку", command=choose_dir)
 btn_dialog.pack(side=LEFT, padx=5)
 
-btn_start = Button(root, text="Start", command=f_start)
+btn_start = ttk.Button(root, text="Start", style="my.TButton", command=f_start)
 btn_start.pack(fill=X, padx=10)
 
 root.mainloop()
