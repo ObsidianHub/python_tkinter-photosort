d9657c23751011013985abd91e75702e170a89fa
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-11-05 16:14:25 +0200

    project check

--- photosort.py
+++ photosort.py
@@ -5,28 +5,31 @@ from tkinter import ttk
 import os
 from datetime import datetime
 
+
 def choose_dir():
-  dir_path = filedialog.askdirectory()
-  e_path.delete(0, END)
-  e_path.insert(0, dir_path)
+    dir_path = filedialog.askdirectory()
+    e_path.delete(0, END)
+    e_path.insert(0, dir_path)
+
 
 def f_start():
-  cur_path = e_path.get()
-  if cur_path:
-    for folder, subfolders, files in os.walk(cur_path):
-      for file in files:
-        path = os.path.join(folder, file)
-        mtime = os.path.getmtime(path)
-        date = datetime.fromtimestamp(mtime)
-        date = date.strftime("%Y-%m-%d")
-        date_folder = os.path.join(cur_path, date)
-        if not os.path.exists(date_folder):
-          os.mkdir(date_folder)
-        os.rename(path, os.path.join(date_folder, file))
-    messagebox.showinfo('Success', 'Сортировка выполнена успешно')
-    e_path.delete(0, END)
-  else:
-    messagebox.showwarning('Warning', 'Выберите папку с фотографиями')
+    cur_path = e_path.get()
+    if cur_path:
+        for folder, subfolders, files in os.walk(cur_path):
+            for file in files:
+                path = os.path.join(folder, file)
+                mtime = os.path.getmtime(path)
+                date = datetime.fromtimestamp(mtime)
+                date = date.strftime("%Y-%m-%d")
+                date_folder = os.path.join(cur_path, date)
+                if not os.path.exists(date_folder):
+                    os.mkdir(date_folder)
+                os.rename(path, os.path.join(date_folder, file))
+        messagebox.showinfo('Success', 'Сортировка выполнена успешно')
+        e_path.delete(0, END)
+    else:
+        messagebox.showwarning('Warning', 'Выберите папку с фотографиями')
+
 
 root = Tk()
 root.title('PhotoSort')
